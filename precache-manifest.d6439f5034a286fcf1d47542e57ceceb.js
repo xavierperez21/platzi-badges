9608946f5c0ecaf7a32c4a0bd194d87580197d75
Xavierperez <xavierpm1221@gmail.com>
2020-08-02 03:08:01 -0500

    Updates

---
+++ precache-manifest.d6439f5034a286fcf1d47542e57ceceb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd5f07ae4bde0ddbcda0",
+    "url": "/Platzi-badges/static/css/main.c8c74e03.chunk.css"
+  },
+  {
+    "revision": "cd5f07ae4bde0ddbcda0",
+    "url": "/Platzi-badges/static/js/main.cd5f07ae.chunk.js"
+  },
+  {
+    "revision": "c8352dc2f1260fcf9a0c",
+    "url": "/Platzi-badges/static/js/runtime~main.c8352dc2.js"
+  },
+  {
+    "revision": "6d61bc7c82a3558a89ab",
+    "url": "/Platzi-badges/static/css/2.9e3a21fb.chunk.css"
+  },
+  {
+    "revision": "6d61bc7c82a3558a89ab",
+    "url": "/Platzi-badges/static/js/2.6d61bc7c.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/Platzi-badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/Platzi-badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/Platzi-badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "178162791bff249ec35b7560f30746ec",
+    "url": "/Platzi-badges/static/media/github_icon.17816279.svg"
+  },
+  {
+    "revision": "a5a2124411a4ec9a1bd78fb4e0644b41",
+    "url": "/Platzi-badges/static/media/linkedin_icon.a5a21244.svg"
+  },
+  {
+    "revision": "b494a10abc05cce555c00324799e1513",
+    "url": "/Platzi-badges/static/media/instagram_icon.b494a10a.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/Platzi-badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/Platzi-badges/static/media/twitter.c9f12008.svg"
+  },
+  {
+    "revision": "f2c49c5714cdf83ade5d3750d94000d1",
+    "url": "/Platzi-badges/static/media/internal_error.f2c49c57.svg"
+  },
+  {
+    "revision": "bc90363b4c46b31304de7afed2fe6796",
+    "url": "/Platzi-badges/static/media/error_404.bc90363b.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Platzi-badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "ca5c9c3d97647cff606d9eb2cc15d15e",
+    "url": "/Platzi-badges/index.html"
+  }
+];
